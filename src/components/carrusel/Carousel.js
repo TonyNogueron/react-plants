5b02940767f06315f7885e8131a90d172b4121c8
ArmandoArV <armando.arredondo.valle@gmail.com>
2022-10-07 08:53:49 -0500

    Added the carousel js

---
+++ src/components/carrusel/Carousel.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+
+
+
+  return (
+    <>
+    <div className="tag">
+          <h1>Image Gallery</h1>
+    </div>
+      <div className="imgslider">
+
+      </div>
+          </>
+  )
+}
+export default ImageSlider;
