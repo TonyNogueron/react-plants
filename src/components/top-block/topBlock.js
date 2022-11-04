91f7de581dff22ad7483a235bc4cba82c4a1a916
ArmandoArV <armando.arredondo.valle@gmail.com>
2022-11-04 00:03:42 -0600

    Mods in HomePage.js and topBlock.js.

--- src/components/top-block/topBlock.js
+++ src/components/top-block/topBlock.js
@@ -1,17 +1,17 @@
 import React from "react";
 import styles from "./top-block.css"
-import plantImg from "../../images/plantDeco.png"
 
 function TopBlock(){
     return(
     <section className="topBlock" id = "topBlock">
         <div className="topBlock-textBox">
-            <img src={plantImg} alt={plantImg}/>
             <h1>What's Planty?</h1>
             <p>Planty is a university project, that has the mission to aid those persons who can't constantly pay attention to their plants. </p>
             <button>About us > </button>
+
         </div>
     </section>
+
     );
 }
 
