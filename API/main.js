a3fb85dea7ee8ac72845df05f23c04cf7ffcf038
ArmandoArV <armando.arredondo.valle@gmail.com>
2022-11-04 10:36:02 -0600

    Mods in main.js

--- API/main.js
+++ API/main.js
@@ -24,3 +24,40 @@ app.use('/', register);
 
 app.listen(port, () => console.log(`Servidor iniciado en http://localhost:${port}`));
 
+app.post('/register', (req, res) => {
+    const { name, email, password } = req.body;
+    console.log(name, email, password);
+    res.send('received');
+    const pool = mysql.createPool({
+        host: 'pruebas-clases-armando.cunjphg47bez.us-east-1.rds.amazonaws.com',
+        user: 'admin',
+        password: process.env.PASSWORD,
+        database: 'Planty',
+        waitForConnections: true,
+        connectionLimit: 10,
+        queueLimit: 0
+    });
+    pool.getConnection((err, connection) => {
+        if (err) {
+            if (err.code === 'PROTOCOL_CONNECTION_LOST') {
+                console.error('Database connection was closed.');
+            }
+            if (err.code === 'ER_CON_COUNT_ERROR') {
+                console.error('Database has too many connections.');
+            }
+            if (err.code === 'ECONNREFUSED') {
+                console.error('Database connection was refused.');
+            }
+        }
+        if (connection) connection.release();
+    });
+    pool.query('INSERT INTO users (name, email, password) VALUES (?, ?, ?)', [name, email, password], (err, rows) => {
+        if (err) {
+            console.log(err);
+        } else {
+            console.log(rows);
+        }
+    }
+    );
+
+});
