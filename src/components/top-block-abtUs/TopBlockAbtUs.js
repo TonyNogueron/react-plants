958195920101d560a44ab1e64dc418bc911d810b
CamiTurner <camilaturner_08@hotmail.com>
2022-11-16 10:44:45 -0600

    return to original

--- src/components/top-block-abtUs/TopBlockAbtUs.js
+++ src/components/top-block-abtUs/TopBlockAbtUs.js
@@ -10,8 +10,7 @@ function TopBlockAbtUs(){
             <h1>Who are we?</h1>
             <p>Planty is a university project that has the mission on helping those people who can't attend their plants. </p>
             <h2>What do we do?</h2>
-            <p>Planty is composed of two parts:an intelligent pot that is able to monitor the status of your plant and a digital service where you
-                 
+            <p>Planty is composed of two parts:an intelligent pot that is able to monitor the status of your plant and a digital service where you     
             can see the data gathered from the sensors that are included in the pot, all this through your mobile device.</p>
             <h2>How do we do it?</h2>
             <p>Planty is a digital service that can water your plants, this through their mobile devices. </p>
