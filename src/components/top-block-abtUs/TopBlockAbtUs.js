43013b0d4f2191d8519e00625666a7bb26a31660
CamiTurner <camilaturner_08@hotmail.com>
2022-11-16 10:27:40 -0600

    top-blockAbtUs paragraph text

--- src/components/top-block-abtUs/TopBlockAbtUs.js
+++ src/components/top-block-abtUs/TopBlockAbtUs.js
@@ -8,9 +8,11 @@ function TopBlockAbtUs(){
     <section className="TopBlockAbtUs" id = "TopBlockAbtUs">
         <div className="TopBlockAbtUs-textBox">
             <h1>Who are we?</h1>
-            <p>Planty is a university project, who has the mission on helping those people who can't attend their plants. </p>
+            <p>Planty is a university project that has the mission on helping those people who can't attend their plants. </p>
             <h2>What do we do?</h2>
-            <p>Planty is a digital service that can water your plants, this through their mobile devices. </p>
+            <p>Planty is composed of two parts:an intelligent pot that is able to monitor the status of your plant and a digital service where you
+                 
+            can see the data gathered from the sensors that are included in the pot, all this through your mobile device.</p>
             <h2>How do we do it?</h2>
             <p>Planty is a digital service that can water your plants, this through their mobile devices. </p>
             <button className="TopBlockAbtUs-button" onClick={() => navigate("/RegisterPage")}>Join us!</button>
