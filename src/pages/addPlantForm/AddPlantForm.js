587116c0b263890aa28fccd6ab0ae55b966b3056
ArmandoArV <armando.arredondo.valle@gmail.com>
2022-12-02 00:14:13 -0600

    si

--- src/pages/addPlantForm/AddPlantForm.js
+++ src/pages/addPlantForm/AddPlantForm.js
@@ -4,9 +4,20 @@ import axios from "axios";
 import { useState } from "react";
 import styles from "./AddPlantForm.css";
 import HeaderSensors from "../../components/header-sensors/HeaderSensors";
+import ImageSlider from "../../components/image-slider/ImageSlider";
 import Footer from "../../components/footer/Footer";
 import url from "../../config/apiConfig";
 import Swal from "sweetalert2";
+
+
+const slidesArray = [
+  {url: 'https://media.admagazine.com/photos/61de539e089751617cd2fc74/16:9/w_2560%2Cc_limit/plantas.jpg', title: 'Plants'},
+  {url: 'https://estaticos-cdn.prensaiberica.es/clip/d0fad945-7822-4c52-8820-d2f5487078cd_16-9-discover-aspect-ratio_default_0.jpg', title: 'Leaves'},
+  {url: 'https://www.clara.es/medio/2022/03/16/plantas-de-interior-bonitas_ad98ca6d_1280x720.jpg', title: 'Cactus'},
+  {url: 'https://quinto-poder.mx/u/fotografias/m/2022/9/28/f608x342-26452_56175_17.jpg', title: 'CactusBonito'},
+  {url: 'https://s1.eestatic.com/2021/03/22/actualidad/567954346_176146463_1706x960.jpg', title: 'Pots'},
+];
+
 
 function AddPlantForm() {
   const navigate = useNavigate();
@@ -58,7 +69,8 @@ function AddPlantForm() {
   return (
     <>
       <HeaderSensors />
-      <section id="register_background">
+      <ImageSlider slides={slidesArray} />
+      <section>
         <div className="container">
           <div className="row">
             <div className="col">
@@ -115,6 +127,7 @@ function AddPlantForm() {
         </div>
       </section>
       <Footer />
+
     </>
   );
 }
---
+++ src/pages/editPlant-page/EditPlant.js
@@ -0,0 +1,6 @@
+import React from "react";
+import styles from "./EditPlant.css";
+
+export default function EditPlant() {
+
+}
